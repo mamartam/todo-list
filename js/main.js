d2afe0c92aef0425713f0d97fd0e4c7835fe6f81
mamartam <marta.masiukk@gmail.com>
2026-02-03 11:45:47 +0200

    change logic of the web page and add interaction with local storage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const taskInput = document.getElementById("task-input"); 
+const taskInput = document.getElementById("task-input");
 const addBtn = document.querySelector(".add-btn");
 const allTasksTodoContainer = document.querySelector(
   ".all-tasks-todo-container",
@@ -9,50 +9,83 @@ const listOfTasks = document.querySelector(".list-of-tasks");
 const doneTasksContainer = document.querySelector(".done-tasks-container");
 const doneTasksList = document.querySelector(".done-tasks-list");
 
-function counter() {
-  const counterOfTasks = document.getElementsByClassName("todo-list-item");
-  amountOfTasks.textContent = Array.from(counterOfTasks).length;
-  const counterOfDoneTasks = document.getElementsByClassName("done-task-item");
-  amountOfDoneTasks.textContent = Array.from(counterOfDoneTasks).length;
+let newOne = JSON.parse(localStorage.getItem("arrayY")) || [];
+amountOfTasks.textContent = newOne.length;
+console.log(newOne);
+// displaying
+
+function displayingList() {
+  listOfTasks.innerHTML = "";
+  newOne.forEach((element, index) => {
+    listOfTasks.innerHTML += `<li class="todo-list-item" id=${index}>${element}<span><img src="images/tick.svg" alt="Tick button" data-id=${index} class="tick-btn"><img src="images/delete.svg" alt="Delete button" class="delete-btn" data-id=${index}></span></li>`;
+    taskInput.value = "";
+  });
+  localStorage.setItem("arrayY", JSON.stringify(newOne));
+  amountOfTasks.textContent = newOne.length;
+}
+displayingList();
+addBtn.addEventListener("click", () => {
+  addNewElement();
+});
+taskInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addNewElement();
+  }
+});
+
+function addNewElement() {
+  if (taskInput.value.trim() !== "") {
+    newOne.push(taskInput.value);
+    displayingList();
+  }
 }
 
-addBtn.addEventListener("click", () => {
-  if (taskInput.value !== "") {
-    const todoListItem = document.createElement("li");
-    todoListItem.classList.add("todo-list-item");
-    listOfTasks.appendChild(todoListItem);
-    todoListItem.textContent = taskInput.value.trim();
-    taskInput.value = "";
-    //--------------------------------------------
-    const spanContainerForImgBtns = document.createElement("span");
-    todoListItem.appendChild(spanContainerForImgBtns);
-    //--------------------------------------------
-    const tickBtn = document.createElement("img");
-    tickBtn.src = "images/tick.svg";
-    tickBtn.alt = "Tick button";
-    tickBtn.classList.add("tick-btn");
-    spanContainerForImgBtns.appendChild(tickBtn);
-    //--------------------------------------------
-    const deleteBtn = document.createElement("img");
-    deleteBtn.src = "images/delete.svg";
-    deleteBtn.alt = "Delete button";
-    deleteBtn.classList.add("delete-btn");
-    spanContainerForImgBtns.appendChild(deleteBtn);
-    //--------------------------------------------
-    counter();
+const deleteBtns = document.getElementsByClassName("delete-btn");
 
-    deleteBtn.addEventListener("click", () => {
-      listOfTasks.removeChild(todoListItem);
-      counter();
-    });
-    tickBtn.addEventListener("click", () => {
-      const doneTaskItem = document.createElement("li");
-      doneTasksList.appendChild(doneTaskItem);
-      doneTaskItem.classList.add("done-task-item");
-      //--------------------------------------------
-      doneTaskItem.textContent = todoListItem.textContent;
-      listOfTasks.removeChild(todoListItem);
-      counter();
-    });
+listOfTasks.addEventListener("click", (item) => {
+  if (item.target.classList.contains("delete-btn")) {
+    let index = item.target.dataset.id;
+    newOne.splice(index, 1);
+    console.log(newOne);
+    displayingList();
+  }
+});
+
+let arrayForDoneTasks =
+  JSON.parse(localStorage.getItem("arrayForDoneTasks")) || [];
+console.log(arrayForDoneTasks);
+
+function displayingListOfDoneItems() {
+  doneTasksList.innerHTML = "";
+  arrayForDoneTasks.forEach((element, index) => {
+    doneTasksList.innerHTML += `<li class="done-task-item" id=${index}>${element}<span><img src="images/restart.svg" alt="Restart button" class="restart-btn" data-id=${index}></span></li>`;
+    taskInput.value = "";
+  });
+
+  localStorage.setItem("arrayForDoneTasks", JSON.stringify(arrayForDoneTasks));
+  amountOfDoneTasks.textContent = arrayForDoneTasks.length;
+}
+displayingListOfDoneItems();
+listOfTasks.addEventListener("click", (item) => {
+  if (item.target.classList.contains("tick-btn")) {
+    console.log(item.target.dataset.id);
+    let index = item.target.dataset.id;
+    let doneItem = newOne.splice(index, 1);
+    arrayForDoneTasks.push(doneItem[0]);
+    displayingListOfDoneItems();
+    displayingList();
+  } else {
+    console.log(false);
+  }
+});
+
+doneTasksList.addEventListener("click", (btn) => {
+  if (btn.target.classList.contains("restart-btn")) {
+    console.log(btn.target.dataset.id);
+    let index = btn.target.dataset.id;
+    let restartItem = arrayForDoneTasks.splice(index, 1);
+    newOne.push(restartItem[0]);
+    displayingListOfDoneItems();
+    displayingList();
   }
 });
